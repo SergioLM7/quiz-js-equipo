93123813004003b0cfd7438e30895802ace46cc0
Nitolez <antonioglezt98@gmail.com>
2024-06-12 23:36:45 +0200

    Style updated y boton de redirección a Questions

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('goToQuiz').addEventListener('click', function() {
+        window.location.href = 'html/questions.html';
+    });
+});
